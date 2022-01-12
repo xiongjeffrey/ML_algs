b51c3c7cf345b6b9d3e678f910d9ff23c4398f1c
xiongjeffrey <67850643+xiongjeffrey@users.noreply.github.com>
2022-01-12 00:15:16 -0800

    Update singleState.py

--- singleState.py
+++ singleState.py
@@ -1,44 +1,10 @@
-def getNewProb(trialMatrix, hiddenLay, k):
-    #!TODO: generalize for more than 2 states
-    # !NOTE: for now, k is 2
+import numpy as np
 
-    # returns new probability vector after hidden layer is applied
-    # returned probability vector has length k, where k := number of states
-
-    # returned probability = weighted average of all trials within a given state
-
-    # guaranteed that all values in hidden vector are integers 0 ~ k
-
-    tempMat = []
-
-    # get the asummed counts for each
-    for i in range(len(trialMatrix)):
-        a_h = hiddenLay[i][0]*trialMatrix[i][0]
-        a_t = hiddenLay[i][0]*trialMatrix[i][1]
-
-        b_h = hiddenLay[i][1]*trialMatrix[i][0]
-        b_t = hiddenLay[i][1]*trialMatrix[i][1]
-
-        tempMat.append([a_h, a_t, b_h, b_t])
-
-    #!TODO: make this more efficient
-    #print(tempMat)
-
-    columnSums = [sum([row[i] for row in tempMat]) for i in range(0,len(tempMat[0]))]
-    #print(columnSums)
-
-    probVect = [0, 0]
-    probVect[0] = columnSums[0]/(columnSums[0] + columnSums[1])
-    probVect[1] = columnSums[2] / (columnSums[2] + columnSums[3])
-
-    return probVect
-
-def getHiddenLayer(trialMatrix, probVector):
+def getProb(trialMatrix, probVector):
     # !TODO: see if can generalize for more than 2 states
 
     # trialVector has length m*2, describing # of heads, # of tails at that trial
-    # returns hiddenLayer of dimensions m*k, where m is number of trials and k is number of states
-    #!NOTE: for now, k is 2
+    # returns new probability vector
 
     # for each trial, find closest element of probVector
     z = []
@@ -60,9 +26,27 @@ def getHiddenLayer(trialMatrix, probVector):
 
         z.append([a, b])
 
-        #print(z)
+    # z is hidden layer of dimensions m*k, where m is number of trials and k is number of states
+    # !NOTE: for now, k is 2
+    z = np.transpose(z)
+    print(z)
 
-    return z
+    # matrix multiplication
+    # matrix returned: each row is a condition (e.g. Coin A, Coin B) for which probabilities are generated
+    #                   each column is a state (e.g. tails, heads) for which each condition can exist in
+    #                   each element is the probability that that condition adopts that state
+    #                   sum of row adds up to 1 (total probabilities of all possible states per condition)
+
+    proportionMatrix = np.matmul(z, trialMatrix)
+    print(proportionMatrix)
+
+    newProb = [0]*len(proportionMatrix)
+
+    #!TODO: generalize this beyond binary, if possible
+    for i in range(len(newProb)):
+        newProb[i] = proportionMatrix[i][0]/sum(proportionMatrix[i])
+
+    return newProb
 
 def hasConverged(arr1, arr2): #!TODO: generalize this for more than 2 states
 
@@ -74,15 +58,12 @@ if __name__ == '__main__':
     trialMatrix = [[5, 5], [9, 1], [8, 2], [4, 6], [7, 3]]
 
     states = 2
-    probVector = [0.8, 0.45]
+    probVector = [0.6, 0.5]
     # probVector = [1.0/states]*states # uniformly initialize an array of number of probability states
 
     print(probVector)
 
-    hidLay = getHiddenLayer(trialMatrix, probVector)
-    print(hidLay)
-
-    newProb = getNewProb(trialMatrix, hidLay, states)
+    newProb = getProb(trialMatrix, probVector)
 
     print(newProb)
 
@@ -90,22 +71,5 @@ if __name__ == '__main__':
 
         probVector = newProb
 
-        hidLay = getHiddenLayer(trialMatrix, probVector)
-        newProb = getNewProb(trialMatrix, hidLay, states)
+        newProb = getProb(trialMatrix, probVector)
         print(newProb)
-
-    # print(newProb)
-
-#!NOTE: NOT USED
-def getClosestElement(k, vect):
-    # returns index of element in vect with value closest to k
-    # in case of tie returns the first one
-
-    minDist = abs(vect[0] - k)
-    minIdx = 0
-    for i in range(1, len(vect)):
-        if abs(vect[i] - k) < minDist:
-            minDist = abs(vect[i] - k)
-            minIdx = i
-
-    return minIdx
